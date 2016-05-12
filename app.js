3828bb60752f046bca7671ac5a428960278e1e9e
knandula <knandula@factset.com>
2016-05-12 16:35:31 +0530

    testing

--- app.js
+++ app.js
@@ -10,39 +10,17 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 
 var port = process.env.PORT || 3000;
-//var user = require('./models/userModel');
-//require('./routes/routes')(app);
 
 var router = express.Router();
 
-//router.route('/user')
-//      .post(function(req,res){
-//        var user1 = new user(req.body);
-//        user1.save();
-//        console.log(user1);
-//        res.send(user1);
-//    })
-//      .get(function(req,res){
-//        var query = {};
-//        if (req.query.name){
-//            query.name = req.query.name;
-//        }
-//       user.find(function(err,users){
-//           if(err)
-//           res.status(500).send(err);
-//           else
-//           res.json(users);
-//       })
-//       // var responsejson ={hell0:"This is my api"};
-//       // res.json(responsejson);
-//    });
 
 router.route('/users/:Id')
 .get(function(req,res) {
-
-        var u = {"name" : "Ravi","Id": 10171};
+        var sentence =  req.params.Id;
+        var msg = null;
+        msg = "breaking test results";
+        var u = {"name" : msg};
         res.json(u);
-
     });
 
 app.use('/api',router);
